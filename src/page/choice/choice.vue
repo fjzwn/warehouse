67faa27fd97de7f4157cc5baed703978973306c6
yutiy <494657028@qq.com>
2018-01-24 17:32:28 +0800

    小仓库项目初始化

---
+++ src/page/choice/choice.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    this is choice page
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'choice',
+  mounted () {
+    console.log(111)
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
